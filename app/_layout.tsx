2002b2ce7dcdbd5ce2aac9e1f8fbefe2b0272bb4
DearDarlin <darinakovalenko78@gmail.com>
2025-11-28 17:06:58 +0200

    i do all my tasks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkAndConvexProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaView>
+    </ClerkAndConvexProvider>
+  );
 }
