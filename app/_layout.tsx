ec054f4ab72c17aae32fb2ca4bde9f93e64a10fb
yaroslavkash <yari4ek16@gmail.com>
2025-11-29 11:11:35 +0100

    third commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,9 @@ import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 
+
+
+
 export default function RootLayout() {
   return (
     <ClerkAndConvexProvider>
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,15 @@
-import { Redirect } from "expo-router";
- 
-
-
+import { Text, View } from "react-native";
 
 export default function Index() {
-  return <Redirect href="/(auth)/login" />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
 }
