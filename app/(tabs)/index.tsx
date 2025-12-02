cb3466df75791e59bf6941011ce2b9eb36004ad3
DimaNaumiv <naumovdima2008@gmail.com>
2025-12-02 18:53:00 +0200

    Changed tabs index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ export default function Feed() {
     const { signOut } = useClerk();
     return (
         <View style={authStyles.container}>
-            <Text style={authStyles.text}>Ти увійшов! Це головна!!!!!!!!!!!!!!!!</Text>
+            <Text style={authStyles.text}>Ти увійшов! Це головна!</Text>
             <Button title="Вийти" color="red" onPress={() => signOut()} />
         </View>
     );
