ed39d9f2cff806b69fa8c9f55ed9adb41be81611
Skhmelyuk <skhmelyuk1985@gmail.com>
2025-12-02 10:01:02 +0200

    review

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function NotificationsScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
-            <Text style={{ fontSize: 20 }}>Сповіщення!!!!</Text>
+            <Text style={{ fontSize: 20 }}>Сповіщення!</Text>
         </View>
     );
 }
