023f1959080c28b48e336a55d290cb55d22247f1
DimaNaumiv <naumovdima2008@gmail.com>
2025-12-17 23:51:57 +0200

    clear

--- app/(tabs)/notifications.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function NotificationsScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#e9bdbdff' }}>
-            <Text style={{ fontSize: 20 }}>Сповіщення!</Text>
-        </View>
-    );
-}
--- constants/theme.ts
+++
@@ -1,8 +0,0 @@
-export const COLORS = {
-  primary: "#1DA1F2", // Twitter Blue
-  background: "#000000", // Чорний фон
-  surface: "#16181C", // Темно-сірий
-  surfaceLight: "#2F3336", // Світліший сірий
-  white: "#E7E9EA", // Білий текст
-  grey: "#71767B", // Сірий
-} as const;
