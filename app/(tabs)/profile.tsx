862a40f2945673e475eab031201d83d6705f44a3
DimaNaumiv <naumovdima2008@gmail.com>
2025-12-18 03:42:17 +0200

    not full profile part

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,13 +1,15 @@
+import { ScreenWrapper } from "@/components/ScreenWrapper";
 import { api } from "@/convex/_generated/api";
 import { styles } from "@/styles/profile.styles";
 import { useAuth } from "@clerk/clerk-expo";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { useMutation, useQuery } from "convex/react";
 import { Image } from 'expo-image';
+import { useRouter } from "expo-router";
 import { useState } from "react";
 import { FlatList, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { Doc } from '../../convex/_generated/dataModel';
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import { useRouter } from "expo-router";
+import { ModalEditProfile } from "@/components/EditProfileModal";
 
 export default function ProfileScreen() {
   const { signOut,userId } = useAuth();
@@ -27,7 +29,8 @@ export default function ProfileScreen() {
   }
 
   return (
-    <View style={styles.box}>
+    <ScreenWrapper>
+    <View>
       <View style={styles.header}>
         <Image style={styles.avatar_image} source={currentUser?.image} contentFit="cover" transition={200}></Image>
         <View style={styles.avatar_box}>
@@ -52,18 +55,19 @@ export default function ProfileScreen() {
       </View>
       <Text style={styles.bio}>{currentUser?.bio}</Text>
       <View style = {styles.buttons}>
-        <TouchableOpacity style={styles.change_button}><Text style={styles.button_text}>Change info</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.logout_button}><Text style={styles.button_text}>Log out</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.share_button}><Text style={styles.button_text}>Share</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.params_button}><FontAwesome6 name="bars" size={20} color="black" /></TouchableOpacity>
+        <TouchableOpacity style={styles.change_button} onPress={()=>setIsModalWindovVisiable(p=>!p)}><Text style={styles.button_text}>Change info</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.logout_button} onPress={()=>signOut()}><Text style={styles.button_text}>Log out</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.params_button} onPress={()=>router.push('/(tabs)/bookmarks')}><FontAwesome6 name="bars" size={20} color="black" /></TouchableOpacity>
       </View>
-      <ScrollView style = {styles.posts}>
-        <FlatList data={posts} keyExtractor={(i)=>i._id} renderItem={(i)=>{
-          return <TouchableOpacity>
+      <ScrollView>
+        <FlatList data={posts} numColumns={3} keyExtractor={(i)=>i._id} renderItem={(i)=>{
+          return <TouchableOpacity style={styles.post_button} onPress={()=>setSelectedPost(i.item)}>
             <Image style={styles.post_image} source={i.item.imageUrl} contentFit="cover" transition={200}></Image>
           </TouchableOpacity>
         }}></FlatList>
       </ScrollView>
+      <ModalEditProfile isEditModalVisible={isModalWindovVisiable} setIsEditModalVisible={setIsModalWindovVisiable} editedProfile={editProfile} setEditedProfile={setEditProfile} handleSaveProfile={handlerSafeProfile}></ModalEditProfile>
     </View>
+    </ScreenWrapper>
   );
 }
--- styles/profile.styles.ts
+++ styles/profile.styles.ts
@@ -2,16 +2,11 @@ import { COLORS } from "@/constants/theme";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    box:{
-
-    },
 
     header:{
         width:'100%',
         alignItems:'center',
         flexDirection:'row',
-        backgroundColor:COLORS.grey,
-        marginTop:40,
         padding:5
     },
     avatar_image:{
@@ -35,7 +30,7 @@ export const styles = StyleSheet.create({
         alignItems:'center'
     },
     avatar_info_text:{
-        fontSize:13,
+        fontSize:15,
         color:COLORS.white
     },
 
@@ -43,7 +38,6 @@ export const styles = StyleSheet.create({
         alignItems:'center',
         flexDirection:'row',
         justifyContent:'center',
-        backgroundColor:COLORS.grey,
         padding:5
     },
     logout_button:{
@@ -78,23 +72,71 @@ export const styles = StyleSheet.create({
         fontSize:17
     },
 
-    posts:{
-        backgroundColor:COLORS.grey
-    },
     post_button:{
-        flex: 1 / 3,
-        aspectRatio: 1,
-        padding: 1,
+        marginTop:10,
+        marginLeft:10
     },
     post_image:{
-        flex: 1,
+        width:112,
+        height:112
     },
 
     bio:{
         fontSize:17,
         fontWeight:'bold',
         color:COLORS.white,
-        backgroundColor:COLORS.grey,
         paddingLeft:15
+    },
+
+
+    modal:{
+        flex:1,
+        backgroundColor:COLORS.white
+    },
+    
+    header_modal:{
+        backgroundColor:COLORS.background,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        padding:7
+    },
+    header_text_modal:{
+        fontWeight:'bold',
+        fontSize:17,
+        color:COLORS.white
+    },
+
+    box_modal:{
+        alignSelf:'center',
+        padding:5,
+        marginTop:10
+    },
+    text_box_modal:{
+        fontSize:23,
+        fontWeight:'bold',
+        marginBottom:5,
+        alignSelf:'center'
+    },
+    textinput_box_modal:{
+        backgroundColor:COLORS.background,
+        borderRadius:5,
+        color:COLORS.white,
+        fontSize:15,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+
+    button_modal:{
+        padding:9,
+        backgroundColor:COLORS.background,
+        borderRadius:5,
+        alignItems:'center',
+        alignSelf:'center',
+        marginTop:10
+    },
+    button_text_modal:{
+        fontSize:17,
+        fontWeight:'bold',
+        color:COLORS.white
     }
 })
